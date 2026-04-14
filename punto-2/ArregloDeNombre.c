dff9203c03cd35bea44ec844264f67888d85e374
Franco3813 <franconicolasaguirre.04@gmail.com>
2026-04-14 20:54:55 -0300

    primera parte del punto 2

---
+++ punto-2/ArregloDeNombre.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void mostrarPersonas(char *nombres[]){
+    printf("Nombres guardados: \n");
+    for (int i = 0; i < 5; i++){
+        printf("%s, ", nombres[i]);
+    }
+    
+}
+
+int main(){
+    char *nombre[5];
+
+    for (int i = 0; i < 5; i++){
+        char buff[50];
+
+        printf("\nEscriba un nombre n%d: ",i+1);
+        scanf("%s",buff); 
+        int tama = strlen(buff);
+
+        nombre[i] = (char *)malloc (tama * sizeof(char) + 1);
+        strcpy(nombre[i], buff);
+    }
+
+    mostrarPersonas(nombre);
+}
