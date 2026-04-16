0769a1449e66999c673a661f62a2e61aa547dea1
Franco3813 <franconicolasaguirre.04@gmail.com>
2026-04-15 21:18:18 -0300

    busqueda por id

--- punto-2/ArregloDeNombre.c
+++ punto-2/ArregloDeNombre.c
@@ -3,20 +3,31 @@
 #include <string.h>
 
 void mostrarPersonas(char *nombres[]){
-    printf("Nombres guardados: \n");
+    printf("\n");
+    printf("Nombres guardados: ");
     for (int i = 0; i < 5; i++){
         printf("%s, ", nombres[i]);
     }
     
 }
 
+void BuscarNombre(int id, char *nombre[]){
+    if (id < 6 && id >= 0){
+        printf("el nombre en la posicion %d es: %s", id, nombre[id-1]);
+    }else{
+        printf("no se encontró el valor buscado");
+    }
+    
+}
+
 int main(){
     char *nombre[5];
+    int id;
 
     for (int i = 0; i < 5; i++){
         char buff[50];
 
-        printf("\nEscriba un nombre n%d: ",i+1);
+        printf("Escriba un nombre n%d: ",i+1);
         scanf("%s",buff); 
         int tama = strlen(buff);
 
@@ -25,4 +36,10 @@ int main(){
     }
 
     mostrarPersonas(nombre);
+
+    printf("\ningrese la posicion del nombre deseado: ");
+    scanf("%d",&id);
+
+    BuscarNombre(id,nombre);
+
 }
