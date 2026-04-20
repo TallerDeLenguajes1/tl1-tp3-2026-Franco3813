1dea00214978c79bad723db9207280dbd175d543
Franco3813 <franconicolasaguirre.04@gmail.com>
2026-04-20 20:30:02 -0300

    punto 3 terminado

--- punto-3/distribuidora.c
+++ punto-3/distribuidora.c
@@ -14,12 +14,18 @@ struct Cliente {
     int ClienteID; // Numerado en el ciclo iterativo
     char *NombreCliente; // Ingresado por usuario
     int CantidadProductosAPedir; // (aleatorio entre 1 y 5)
-    struct Producto *Productos //El tamaño de este arreglo depende de la variable “CantidadProductosAPedir”
+    struct Producto *Productos; //El tamaño de este arreglo depende de la variable “CantidadProductosAPedir”
 };
+
+int calcular(int cant,int precio){
+    int a;
+    a = cant * precio;
+    return a;
+}
 
 int main(){
     char *TiposProductos[]={"Galletas","Snack","Cigarrillos","Caramelos","Bebidas"};
-    int cantClientes,a;
+    int cantClientes,a,cantP,precio,total;
     struct Cliente datos[5];
     srand(time(NULL));
 
@@ -28,7 +34,9 @@ int main(){
 
     if (cantClientes > 0  && cantClientes < 6){
         for (int i = 0; i < cantClientes; i++){
+            int totalA;
             char buff[50];
+            totalA = 0;
             datos[i].ClienteID = i+1;
 
             printf("\nIngrese el nombre del usuario n%d: ",i+1);
@@ -43,17 +51,24 @@ int main(){
 
             for (int j = 0; j < datos[i].CantidadProductosAPedir; j++){
                 datos[i].Productos[j].ProductoID = j+1;
-                datos[i].Productos[j].Cantidad = 1 + rand()%9;
-                a = rand()%4;
+                datos[i].Productos[j].Cantidad = 1 + rand()%10;
+                a = rand()%5;
                 datos[i].Productos[j].TipoProducto = TiposProductos[a];
-                datos[i].Productos[j].PrecioUnitario = 10 + rand()%90;
+                datos[i].Productos[j].PrecioUnitario = 10 + rand()%91;
+
+                cantP = datos[i].Productos[j].Cantidad;
+                precio = datos[i].Productos[j].PrecioUnitario;
+                total = calcular(cantP,precio);
+                totalA = totalA + total;
 
                 printf("\nNumero de cliente: %d",i+1);
                 printf("\nProducto id: %d",datos[i].Productos[j].ProductoID);
                 printf("\nCantidad del producto: %d",datos[i].Productos[j].Cantidad);
                 printf("\nTipo de producto: %s",datos[i].Productos[j].TipoProducto);
-                printf("\nPrecio: %d\n",datos[i].Productos[j].PrecioUnitario);
+                printf("\nPrecio Unitario: %.2f",datos[i].Productos[j].PrecioUnitario);
+                printf("\nPrecio total: %d\n",total);
             }
+            printf("Total a pagar: %d",totalA);
         }
         
     }else{
